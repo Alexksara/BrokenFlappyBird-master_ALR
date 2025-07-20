ce92cd7b099384406f2f3b5b13689096a75d21ba
Alex Rodriguez <alex.rodriguez.373@gmail.com>
2025-07-20 16:53:57 -0500

    Fixed variable names within pipespawner fixed pipe spawning, spawns 1 each second

--- Assets/Script/PipeSpawner.cs
+++ Assets/Script/PipeSpawner.cs
@@ -4,10 +4,10 @@ using System.Collections.Generic;
 
 public class PipeSpawner : MonoBehaviour
 {
-    public GameObject prefab;
-    public float spawnRate = .1f;
-    public float randomVariable = 1f;
-    public Transform wherewewanttheprefabtostart;
+    public GameObject pipePrefab;
+    public float spawnRate = 1f;
+    public float randomRange = 1f;
+    public Transform startSpawn;
     public List<GameObject> pipes;
     
     private float timer = float.MaxValue;
@@ -29,8 +29,8 @@ public class PipeSpawner : MonoBehaviour
 
     void SpawnPipes()
     {
-        float yOffset = Random.Range(-randomVariable, randomVariable);
-        Vector3 spawnPosition = wherewewanttheprefabtostart.position + Vector3.up * yOffset;
-        pipes.Add(Instantiate(prefab, spawnPosition, Quaternion.identity));
+        float yOffset = Random.Range(-randomRange, randomRange);
+        Vector3 spawnPosition = startSpawn.position + Vector3.up * yOffset;
+        pipes.Add(Instantiate(pipePrefab, spawnPosition, Quaternion.identity));
     }
 }
